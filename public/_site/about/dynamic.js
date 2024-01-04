ca2b693b1caf9879260f43cfba2e121d0e87f17d
kpath1999 <kausarpatheryaa@gmail.com>
2024-01-04 21:00:46 +0530

    ahh

---
+++ public/_site/about/dynamic.js
@@ -0,0 +1,20 @@
+const options = {
+    method: 'POST',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  };
+    
+  fetch('https://56uump58ii.execute-api.us-east-1.amazonaws.com/dev/', options)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('Network Response Error');
+      }
+    })
+    .then(data => {
+      document.querySelector('span.visits').insertAdjacentHTML('beforeend', data);
+    })
+    .catch(error => console.log('Error: ', error));
